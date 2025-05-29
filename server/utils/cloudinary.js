870d37056daf7645a0deefb6d98fa7fe3e7e64e5
Rishabhkasyap <rishabhkumar7.rs@gmail.com>
2025-05-29 17:33:49 +0530

    first commit

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+dotenv.config();
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+});
+
+export const uploadMedia = async (file) => {
+  try {
+    const uploadResponse = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+    return uploadResponse;
+  } catch (error) {
+    console.log("Upload Error:", error);
+    return null;
+  }
+};
+
+export const deleteMediaFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId);
+  } catch (error) {
+    console.log("Delete Image Error:", error);
+  }
+};
+
+export const deleteVideoFromCloudinary = async (publicId) => {
+  try {
+    await cloudinary.uploader.destroy(publicId, { resource_type: "video" });
+  } catch (error) {
+    console.log("Delete Video Error:", error);
+  }
+};
